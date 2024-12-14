82846d3a017c005a9ff06e6b6facfcf6dffc4571
Jhonatan Henkel <96666648+Jhon-Henkel@users.noreply.github.com>
2024-12-14 15:57:25 -0300

    Update config-system.sh

--- config-system.sh
+++ config-system.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Configurar atalho Super+V para abrir o Diodon
-install_package dbus-x11
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "[]"
 
-echo "Configurando o tema e doca do Ubuntu para o tema escuro..."
-# Configurar o tema do Ubuntu para o tema escuro
+echo "===================================================================================="
+echo "Configurando Tema..."
+echo "===================================================================================="
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
-# Configurar doca
+echo "===================================================================================="
+echo "Configurando Doca..."
+echo "===================================================================================="
 gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
 gsettings set org.gnome.shell.extensions.dash-to-dock panel-mode false
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
@@ -18,9 +19,12 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 gsettings set org.gnome.shell favorite-apps "['application://gnome-terminal.desktop', 'application://nautilus.desktop', 'application://libreoffice-writer.desktop']"
 
-echo "Tema e doca configurada com sucesso."
 
-# Criar e definir a pasta 'projects' como marcador
-mkdir -p ~/projects
-echo "file:///home/$USER/projects" | sudo tee -a /home/$USER/.config/gtk-3.0/bookmarks > /dev/null
+echo "===================================================================================="
+echo "Criando pasta projects e criando Makefile..."
+echo "===================================================================================="
+mkdir -p ~/home/$USER/projects
+echo "file:///home/$USER/projects" | tee -a /home/$USER/.config/gtk-3.0/bookmarks > /dev/null
 cp ./Makefile ~/home/$USER/Makefile
+sudo chmod 777 -R /home/$USER/projects/
+sudo chmod 777 -R /home/$USER/Makefile
